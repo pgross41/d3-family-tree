08f226a22012b21a69471faa85e10414e12b1623
Patrick Gross <pgross41@gmail.com>
2019-07-31 01:08:37 -0500

    Add leafImgs

--- src/tree.js
+++ src/tree.js
@@ -1,3 +1,4 @@
+import config from '../config.js';
 import styles from './tree.css';
 import RootNode from './root-node';
 import TreeNode from './tree-node';
@@ -13,7 +14,9 @@ class Tree {
 
 	render() {
 		const nodes = this.getNodes();
+		const leafImages = this.getLeafImages();
 		this.el.className = styles.tree;
+		nodes.appendChild(leafImages);
 		this.el.appendChild(nodes);
 		return this.el;
 	}
@@ -39,7 +42,37 @@ class Tree {
 			});
 		}
 		appendChildren(children);
+
 		return nodesWrapper
+	}
+
+
+	// Decorative leaves
+	getLeafImages() {
+		const imgWrapper = document.createElement('div')
+		imgWrapper.className = styles.leafImgs
+		Object.keys(this.metadata.depthCounts).forEach((depth) => {
+			const leavesInRow = Math.round(depth * 6);
+			// const leavesInRow = 10;
+			console.log(leavesInRow);
+			[...Array(leavesInRow)].forEach((_, idx) => {
+				const img = document.createElement('img')
+				img.className = styles.leafImg;
+				img.src = `/images/leaves${idx % 3 + 1}.png`;
+				const thetaStart = Math.PI + (Math.PI - config.maxAngle) / 2;
+				const theta = thetaStart + (config.maxAngle / leavesInRow) * (idx + 0.5) ;
+				const r = ((100 / this.metadata.depthCounts.length) * depth) + 9;
+				console.log(r);
+				const x = (r * Math.cos(theta)) / 2 + 50; // Half because it goes left AND right
+				const y = (r * Math.sin(theta)) + 100; // Not half because it only goes up
+				const rotation = theta + Math.PI/2;
+				img.style.left = `${x}%`;
+				img.style.top = `${y}%`;
+				img.style.transform = `rotate(${rotation}rad)`
+				imgWrapper.appendChild(img);
+			});
+		});
+		return imgWrapper;
 	}
 }
 
